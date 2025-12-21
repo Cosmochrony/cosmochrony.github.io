83d85345470434f37b7981177485a1531d0466c4
javarome <javarome@gmail.com>
2025-12-21 13:06:53 +0100

    Added German localization with `index_de.html`, extended redirection logic in `script.js` for German (`de`), updated language alternates in all HTML files, and adjusted `package.json` metadata.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 export function redirectIfNot(pageLang) {
   const lang = navigator.language.split("-")[0]
   if (lang !== pageLang) {
-    const langSuffix = ["it", "es", "fr"].includes(lang) ? `_${lang}` : ""
+    const langSuffix = ["it", "es", "fr", "de"].includes(lang) ? `_${lang}` : ""
     let redirect = `/index${langSuffix}.html`
     if (redirect !== window.location.pathname) {
       window.location.replace(redirect)
